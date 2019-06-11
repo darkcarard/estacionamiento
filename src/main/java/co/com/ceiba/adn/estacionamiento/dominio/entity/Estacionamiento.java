c15939cf2ffe2142721ba612ea1bc2d5e6b99811
Carlos Javier Ardila Bohorquez - Ceiba Software <carlos.ardila@ceiba.com.co>
2019-06-11 12:23:46 -0500

    Pruebas unitarias para el estacionamiento

--- src/main/java/co/com/ceiba/adn/estacionamiento/dominio/entity/Estacionamiento.java
+++ src/main/java/co/com/ceiba/adn/estacionamiento/dominio/entity/Estacionamiento.java
@@ -1,5 +1,6 @@
 package co.com.ceiba.adn.estacionamiento.dominio.entity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
@@ -12,18 +13,26 @@ import lombok.NoArgsConstructor;
 public class Estacionamiento {
 
 	private static final String MENSAJE_CUPO_LLENO = "Ya no hay cupo para el tipo de vehículo";
+	private static final String MENSAJE_INGRESO_PLACA_NO_AUTORIZADA = "El vehículo no está autorizado para ingresar al estacionamiento el día de hoy";
 
 	private List<Ticket> tickets;
 
-	public void ingresarVehiculo(Vehiculo vehiculo) {
+	public Ticket ingresarVehiculo(Vehiculo vehiculo, LocalDateTime fechaIngreso) {
 
 		EstacionamientoValidator.validarCupo(obtenerCantidadXTipoVehiculo(vehiculo.getTipo()),
 				vehiculo.getTipo().getCantidadMaxima(), MENSAJE_CUPO_LLENO);
+		EstacionamientoValidator.validarIngresoPorPlaca(vehiculo.getPlaca(), fechaIngreso,
+				MENSAJE_INGRESO_PLACA_NO_AUTORIZADA);
+
+		Ticket ticket = new Ticket();
+		ticket.setFechaIngreso(fechaIngreso);
+		ticket.setVehiculo(vehiculo);
+
+		return ticket;
+
 	}
 
-	public long obtenerCantidadXTipoVehiculo(TipoVehiculoEnum tipoVehiculo) {
-
+	private long obtenerCantidadXTipoVehiculo(TipoVehiculoEnum tipoVehiculo) {
 		return tickets.stream().filter(t -> t.getVehiculo().getTipo().equals(tipoVehiculo)).count();
 	}
-
 }
--- src/test/java/co/com/ceiba/adn/estacionamiento/test/dominio/unit/EstacionamientoTest.java
+++ src/test/java/co/com/ceiba/adn/estacionamiento/test/dominio/unit/EstacionamientoTest.java
@@ -2,6 +2,8 @@ package co.com.ceiba.adn.estacionamiento.test.dominio.unit;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,25 +12,107 @@ import org.junit.Test;
 
 import co.com.ceiba.adn.estacionamiento.dominio.entity.Estacionamiento;
 import co.com.ceiba.adn.estacionamiento.dominio.entity.Ticket;
+import co.com.ceiba.adn.estacionamiento.dominio.entity.Vehiculo;
+import co.com.ceiba.adn.estacionamiento.dominio.exception.EstacionamientoException;
+import co.com.ceiba.adn.estacionamiento.test.dominio.BasePrueba;
 import co.com.ceiba.adn.estacionamiento.test.dominio.databuilder.EstacionamientoTestDataBuilder;
+import co.com.ceiba.adn.estacionamiento.test.dominio.databuilder.TicketTestDataBuilder;
+import co.com.ceiba.adn.estacionamiento.test.dominio.databuilder.VehiculoTestDataBuilder;
 
 public class EstacionamientoTest {
 
-	private static final List<Ticket> DEFAULT_TICKETS = new ArrayList<>();
-	
+	private static final LocalDateTime FECHA_INGRESO = LocalDateTime.now();
+	//Esta fecha es un domingo
+	private static final LocalDateTime FECHA_INGRESO_AUTORIZADO = LocalDateTime.of(2019, Month.JUNE, 9, 10, 0);
+	//Esta fecha es un martes
+	private static final LocalDateTime FECHA_INGRESO_NO_AUTORIZADO = LocalDateTime.of(2019, Month.JUNE, 11, 10, 0);
+	private static final String PLACA_EMPIEZA_POR_A = "ABC123";
+	private static final String PLACA_NO_EMPIEZA_POR_A = "BCD123";
+	private static final String MENSAJE_CUPO_LLENO = "Ya no hay cupo para el tipo de vehículo";
+	private static final String MENSAJE_INGRESO_PLACA_NO_AUTORIZADA = "El vehículo no está autorizado para ingresar al estacionamiento el día de hoy";
+
 	private Estacionamiento estacionamiento;
-	
+	private Vehiculo vehiculo;
+
 	@Before
 	public void setUp() {
 		EstacionamientoTestDataBuilder estacionamientoTestDataBuilder = new EstacionamientoTestDataBuilder();
+		VehiculoTestDataBuilder vehiculoTestDataBuilder = new VehiculoTestDataBuilder();
 		estacionamiento = estacionamientoTestDataBuilder.build();
+		vehiculo = vehiculoTestDataBuilder.build();
 	}
-	
+
 	/**
-	 * Probamos que al instanciar un estacionamiento la lista de tickets se instancie también
+	 * Probamos que al instanciar un estacionamiento la lista de tickets se
+	 * instancie también
 	 */
 	@Test
 	public void estacionamientoBuild() {
 		assertNotNull(estacionamiento);
+	}
+
+	/**
+	 * Probamos que se cree un ticket correctamente al ingresar un vehículo y haya
+	 * cupo para el tipo de vehículo. Se garantiza que hay cupo puesto que la lista
+	 * de tickets está vacía.
+	 */
+	@Test
+	public void ingresarVehiculoConCuposDisponibles() {
+		Ticket ticket;
+		VehiculoTestDataBuilder vehiculoTestDataBuilder = new VehiculoTestDataBuilder().withPlaca(PLACA_NO_EMPIEZA_POR_A);
+		vehiculo = vehiculoTestDataBuilder.build();
+		ticket = estacionamiento.ingresarVehiculo(vehiculo, FECHA_INGRESO_AUTORIZADO);
+		assertNotNull(ticket);
+	}
+
+	/**
+	 * Probamos que se muestre un mensaje de error si no hay cupo para el tipo de
+	 * vehículo
+	 */
+	@Test
+	public void ingresarVehiculoSinCuposDisponibles() {
+		TicketTestDataBuilder ticketTestDataBuilder = new TicketTestDataBuilder();
+		List<Ticket> tickets = new ArrayList<>();
+		Ticket ticket;
+		for (int i = 0; i < vehiculo.getTipo().getCantidadMaxima(); i++) {
+			ticket = ticketTestDataBuilder.withId(i).withVehiculo(vehiculo).build();
+			tickets.add(ticket);
+		}
+		estacionamiento.setTickets(tickets);
+		BasePrueba.assertThrows(() -> estacionamiento.ingresarVehiculo(vehiculo, FECHA_INGRESO),
+				EstacionamientoException.class, MENSAJE_CUPO_LLENO);
+
+	}
+	
+	@Test
+	public void ingresarVehiculoConPlacaAEnDiaAutorizado() {
+		VehiculoTestDataBuilder vehiculoTestDataBuilder = new VehiculoTestDataBuilder().withPlaca(PLACA_EMPIEZA_POR_A);
+		vehiculo = vehiculoTestDataBuilder.build();
+		Ticket ticket = estacionamiento.ingresarVehiculo(vehiculo, FECHA_INGRESO_AUTORIZADO);
+		assertNotNull(ticket);
+	}
+	
+	@Test
+	public void ingresarVehiculoConPlacaAEnDiaNoAutorizado() {
+		VehiculoTestDataBuilder vehiculoTestDataBuilder = new VehiculoTestDataBuilder().withPlaca(PLACA_EMPIEZA_POR_A);
+		vehiculo = vehiculoTestDataBuilder.build();
+		BasePrueba.assertThrows(() -> estacionamiento.ingresarVehiculo(vehiculo, FECHA_INGRESO_NO_AUTORIZADO),
+				EstacionamientoException.class, MENSAJE_INGRESO_PLACA_NO_AUTORIZADA);
+	}
+	
+	@Test
+	public void ingresarVehiculoConPlacaNoAEnDiaAutorizado() {
+		VehiculoTestDataBuilder vehiculoTestDataBuilder = new VehiculoTestDataBuilder().withPlaca(PLACA_NO_EMPIEZA_POR_A);
+		vehiculo = vehiculoTestDataBuilder.build();
+		Ticket ticket = estacionamiento.ingresarVehiculo(vehiculo, FECHA_INGRESO_AUTORIZADO);
+		assertNotNull(ticket);
+	}
+	
+	@Test
+	public void ingresarVehiculoConPlacaNoAEnDiaNoAutorizado() {
+		VehiculoTestDataBuilder vehiculoTestDataBuilder = new VehiculoTestDataBuilder().withPlaca(PLACA_NO_EMPIEZA_POR_A);
+		vehiculo = vehiculoTestDataBuilder.build();
+		Ticket ticket = estacionamiento.ingresarVehiculo(vehiculo, FECHA_INGRESO_NO_AUTORIZADO);
+		assertNotNull(ticket);
 	}
 }
